0d09eb1bb85cad4b2ae3cb561935cdf342d9776f
Daniel Rodrigues <105395691+danfrodrigues@users.noreply.github.com>
2026-06-11 00:54:53 -0300

    feat: criar api e autenticacao do frontend

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,8 @@
+import { api } from './api.js'
+
+export const authService = {
+  login: (email, senha, lembrar) => api.post('/login', { email, senha, lembrar }),
+  cadastrar: (dados) => api.post('/cadastro', dados),
+  logout: () => api.post('/logout'),
+  eu: () => api.get('/api/me'),
+}
